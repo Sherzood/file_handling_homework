d4abda452fd057a1600ea2ffb5d4d3d4c985f9ac
sherzod89 <sherzodms@inbox.ru>
2022-04-10 11:08:06 +0300

    01

--- file05.py
+++ file05.py
@@ -6,5 +6,16 @@ def main(data:str):
     Returns:
         list: return answer
     """
+    count_d=0
+    count_s=0
     
+    for i in data:
+        if i.isdigit():
+            count_d+=1
+        else:
+            count_s+=1   
+    return [count_d,count_s]
 # Read data from file
+f=open('txt_file\data05.txt')
+data=f.read()
+print(main(data))
