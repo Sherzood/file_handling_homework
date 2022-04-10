873d320506858eda0f5e62a698cb1d4f4d7365c3
sherzod89 <sherzodms@inbox.ru>
2022-04-10 11:37:14 +0300

    01

--- file09.py
+++ file09.py
@@ -6,5 +6,14 @@ def main(data:str):
     Returns:
         int: return answer
     """
+    mn=0
+    for i in data:
+        if i.isdigit():
+            if int(i)<mn:
+                mn=int(i)
+    return mn            
 
 # Read data from file
+f=open('txt_file\data09.txt')
+data=f.read()
+print(main(data))
